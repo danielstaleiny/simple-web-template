21fa5578e5665e2ebb224348ca0725a2fbe3ab69
danielstaleiny <danielstaleiny@tutanota.com>
2021-09-15 11:58:24 +0200

    update

--- src/_pure/Main.purs
+++ src/_pure/Main.purs
@@ -1,12 +1,15 @@
 module Main where
 
+import Prelude
+
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude
+import Placeholder (Template(..), templateInject)
+
+
 
 main :: Effect Unit
 main = do
-  log "Hello, World!"
-  log "Hello, World!2"
-  log "Hello, World!3"
-  log "Hello, World!"
+  log "works log"
+  log $ templateInject (Test "<p>[[test]]</p>" {test: "hello", so: "oau"})
+  log $ templateInject (Other "<p>[[other]]</p>" {other: "other txt", extra: "extra"})
