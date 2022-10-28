8edf5a62a8e3f3659a3b94bdca9c807fd2f90be4
Daniel Rafaj <daniel.rafaj@tuta.io>
2022-10-28 10:47:50 +0200

    exampl of running capability pattern with Either cancellation.

--- src/_pure/Main.purs
+++ src/_pure/Main.purs
@@ -2,14 +2,20 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Maybe.Trans (MaybeT(..), runMaybeT)
-import Data.Maybe (Maybe(..))
+import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Control.Monad.Except (ExceptT, runExceptT)
+import Control.Monad.Reader (ReaderT, runReaderT)
+import Control.Plus (empty)
+import Data.Either (Either(..))
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff)
+import Effect.Aff (Aff, error, launchAff, never)
 import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Console as Console
 import Events.AddEventListeners (addEventListeners)
+
+
+
 
 -- (<*>) apply  :: f (a -> b) -> f a -> f b
 -- (<$>) map  :: (a -> b) -> f a -> f b
@@ -21,8 +27,9 @@ import Events.AddEventListeners (addEventListeners)
 main :: Effect Unit
 main = do
   -- log "PS Main loaded"
+  let env = { someValue: 2 }
   addEventListeners
-  void <<< launchAff $ runApp program
+  void <<< launchAff $ runApp program env
   pure unit
 
 newtype Name = Name String
@@ -47,17 +54,19 @@ class (Monad m) <= GetValue m where
 -- which makes it easier to test
 program :: forall m. -- AppM
           MonadAff m =>
+          MonadThrow String m =>
           LogToScreen m =>
           GetValue m =>
           m Unit
 program = do
   log "What is your name?"
   name <- get
-  log $ "You name is " <> name 
+  log $ "You name is " <> name
 
 -- Layer 2 (Production)
+type Environment = { someValue :: Int }
 
-newtype AppM a = AppM (MaybeT Aff a)
+newtype AppM a = AppM (ReaderT Environment ( ExceptT String Aff) a)
 derive newtype instance functorTestM    :: Functor AppM
 derive newtype instance applyAppM       :: Apply AppM
 derive newtype instance Applicative AppM
@@ -65,16 +74,30 @@ derive newtype instance bindAppM        :: Bind AppM
 derive newtype instance monadAppM       :: Monad AppM
 derive newtype instance monadEffectAppM     :: MonadEffect AppM
 derive newtype instance monadAffAppM        :: MonadAff AppM
+derive newtype instance monadErrorAppM :: MonadThrow String AppM
 
-runApp :: forall a. AppM a -> Aff (Maybe a)
-runApp (AppM maybe_T) = runMaybeT maybe_T
-
+runApp :: forall a. Show a => AppM a -> Environment -> Aff Unit
+runApp (AppM reader_T) env = do
+  res <- runExceptT $ runReaderT reader_T env
+  case res of
+    (Right u) -> do
+      pure unit
+    (Left err) -> do
+      -- TODO emit error with error event handler
+      liftEffect $ Console.log err
+      pure unit
 -- Layer 1 (the implementations of each instance)
 instance LogToScreen AppM where
   log = liftEffect <<< Console.log
 
+-- instance monadAskAppMA :: TypeEquals e Environment => MonadAsk e AppMA where
+--   ask = AppMA $ asks from
+
 instance GetValue AppM where
-  get = AppM (MaybeT (pure Nothing))
+  get = pure "Daniel"
+    -- throwError "hello"
+    -- ?h $ pure $ (Left "Hello" :: Either String String)
+
 
 -- instance GetUserName AppM where
 --   getUserName = liftEffect do
