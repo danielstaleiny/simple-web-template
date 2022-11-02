e43fac904b5660699d7db78f5376d42c828d85e1
Daniel Rafaj <daniel.rafaj@tuta.io>
2022-11-02 23:17:23 +0100

    .

--- src/_pure/Main.purs
+++ src/_pure/Main.purs
@@ -21,6 +21,39 @@ import Web.HTML.Event.EventTypes as EventType
 -- (>>=) bind  :: m a -> (a -> m b) -> m b
 -- (>=>) kleisly arrow  :: (a -> m b) -> (b -> m c) -> a -> m c
 
+clickEventListeners :: Event -> Effect Unit
+clickEventListeners evt = launchAff_ do
+  let matches_ q = matches evt q
+  let appM fn ev = runApp (fn ev) environment
+  let ifThen_ predicate fn = predicate >>= ifThen (appM fn)
+  -- Cancelable events. We run new AppM for each event.
+  -- This way One event cannot cancel other events.
+  matches_ "[click='click']" `ifThen_` click
+
+main :: Effect Unit
+main = addEventListeners
+  -- Needs class capability for each page, hase its own list of the event handler.
+  -- Each event handler runs our custom Monad stack
+  -- Each page defines UI capability class which defines methods available in that view,
+  -- this UI class is then passed to each eventhandler custom runApp method.
+  -- This way we separate load and separate logic for each page but re-use most of the code on each page.
+  -- Resould to be expect is lower js output on each page.
+  -- Preferably all the utilities should be its own module to be able to re-use the utils.
+
+addEventListeners :: Effect Unit
+addEventListeners = do
+  -- Click event listeners
+  addEventListener EventType.click =<< eventListener clickEventListeners
+  -- Custom event listeners
+  addEventListener TemplateUtils.customEventType =<< eventListener customEventListeners
+
+class (Monad m) <= TemplateIndex m where
+  ui :: { name :: String } -> m Unit
+  list :: Array { name :: String } -> m Unit
+
+instance TemplateIndex AppM where
+  ui obj = liftAff $ customEvent {type_input: "record", box: "#box-click", template: "#template-click", type_: "template"} obj
+  list obj = liftAff $ customEvent {type_input: "array", box: "#box-list", template: "#template-list", type_: "template"} obj
 
 
 
@@ -42,44 +75,3 @@ click _ = do
   list [{name: "List UI"}, {name: "eyoo"}]
 
 
-clickEventListeners :: Event -> Effect Unit
-clickEventListeners evt = launchAff_ do
-  let matches_ q = matches evt q
-  let appM fn ev = runApp (fn ev) environment
-  let ifThen_ predicate fn = predicate >>= ifThen (appM fn)
-  -- Cancelable events. We run new AppM for each event.
-  -- This way One event cannot cancel other events.
-  matches_ "[click='click']" `ifThen_` click
-
-
-addEventListeners :: Effect Unit
-addEventListeners = do
-  -- Click event listeners
-  addEventListener EventType.click =<< eventListener clickEventListeners
-  -- Custom event listeners
-  addEventListener TemplateUtils.customEventType =<< eventListener customEventListeners
-
-main :: Effect Unit
-main = addEventListeners
-  -- Needs class capability for each page, hase its own list of the event handler.
-  -- Each event handler runs our custom Monad stack
-  -- Each page defines UI capability class which defines methods available in that view,
-  -- this UI class is then passed to each eventhandler custom runApp method.
-  -- This way we separate load and separate logic for each page but re-use most of the code on each page.
-  -- Resould to be expect is lower js output on each page.
-  -- Preferably all the utilities should be its own module to be able to re-use the utils.
-  --
-
-
-
-class (Monad m) <= TemplateIndex m where
-  ui :: { name :: String } -> m Unit
-  list :: Array { name :: String } -> m Unit
-
-
-instance TemplateIndex AppM where
-  ui obj = liftAff $ customEvent {type_input: "record", box: "#box-click", template: "#template-click", type_: "template"} obj
-  list obj = liftAff $ customEvent {type_input: "array", box: "#box-list", template: "#template-list", type_: "template"} obj
-
-    -- throwError "hello"
-    -- ?h $ pure $ (Left "Hello" :: Either String String)
